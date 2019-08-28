80056e64d7585e4f37e384d318c8d64866fab8d7
bobbidigi <bobbidigi34@gmail.com>
2019-08-28 13:22:02 -0400

    add one request on mount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
 import "./App.css";
+import PhotoCard from './components/PhotoCard'
 
 function App() {
+
+  const [cardState, cardSetter] = useState([])
+
+  useEffect(() => {
+    // Make a request for a user with a given ID
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then( res =>{
+        // handle success
+        console.log(res);
+        cardSetter(res.data);
+      }
+      )
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  },[])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+     <PhotoCard 
+      title={cardState.title}
+      url={cardState.url}
+      explanation={cardState.explanation}
+     />
     </div>
   );
 }
