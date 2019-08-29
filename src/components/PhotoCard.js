be549ea360cb95fa46a4481dbea4d0101d65c8c8
bobbidigi <bobbidigi34@gmail.com>
2019-08-29 17:18:39 -0400

    add card component from semantic-ui

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './photoCard.scss'
+import {Card} from 'semantic-ui-react';
 
 function PhotoCard(props){
   return (
-    <div className="photo-card">
+    <Card className="photo-card">
       <h2>{props.title}</h2>
       <img src={props.url} alt="nasa"/>
       <p>{props.explanation}</p>
       <p>{props.date}</p>
-    </div>
+    </Card>
   )
 
 }
